986f813da279f6a7e2b3c4cbca3610e17fb4afba
Anthony Pinto <anthonyepinto@gmail.com>
2014-08-09 21:14:47 -0700

    Completed Word-Chainer script

--- word_chains2.rb
+++ word_chains2.rb
@@ -1,6 +1,57 @@
 class WordChainer
   
-  def initialize dictionary_file_name
-    
+  def initialize dictionary_file_name = 'dictionary.txt'
+    @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
+    @all_seen_words = {}
+  end
   
+  def build_path start, target
+    explore start, target
+    word = target
+    [].tap do |path|
+      until path[0] == start
+        path.unshift(word)
+        #works backward from targer using each word's 'parent' 
+        word = @all_seen_words[word]
+      end
+    end
+  end
+  
+  def explore start, target
+    @all_seen_words = {start => nil}
+    current_words = [start]
+    until current_words.empty?
+      base_word = current_words.shift
+      legal_steps(base_word).each do |word|
+        @all_seen_words[word] = base_word
+        return if word == target
+        current_words <<  word
+      end
+    end
+  end
+  
+  def legal_steps base_word
+    adj_words = @dictionary.select do |possible_word|
+      next if word_seen?(possible_word)
+      next if possible_word.length != base_word.length
+      words_adjacent?(base_word, possible_word)
+    end
+    adj_words 
+  end
+  
+  def words_adjacent?(word1, word2)
+    diffs = 0
+    word1.each_char.with_index do |char, index|
+      diffs += 1 unless word2[index] == char
+    end
+    diffs == 1
+  end
+  
+  def word_seen?(word)
+    @all_seen_words.include?(word)
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  p WordChainer.new.build_path('bear', 'wolf')
 end
